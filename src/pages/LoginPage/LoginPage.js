b95192b664b2bf1ac4bfd041f18a1bfcaf81bd58
imanuel <toswim100@gmail.com>
2021-05-09 00:13:44 +0300

    finish desighn login page

--- src/pages/LoginPage/LoginPage.js
+++ src/pages/LoginPage/LoginPage.js
@@ -7,14 +7,14 @@ import UserModel from '../../model/UserModel';
 import airplane from '../../assets/airplane-48.png';
 
 
-function LoginPage({activeUser, onLogin}) {
+function LoginPage({ activeUser, onLogin }) {
     const [email, setEmail] = useState();
     const [pass, setPass] = useState();
     const [showInvalidLogin, setShowInvalidLogin] = useState(false);
 
 
     //when logout users will get out to the Home page
-    if(activeUser){
+    if (activeUser) {
         return <Redirect to="/" />
     }
 
@@ -28,7 +28,7 @@ function LoginPage({activeUser, onLogin}) {
             // Do stuff after successful login
             console.log('Logged in user', parseUser);
             const activeUser = new UserModel(parseUser);
-            console.log('user of UserModel',activeUser);
+            console.log('user of UserModel', activeUser);
             onLogin(activeUser);
         }).catch(error => {
             setShowInvalidLogin(true);
@@ -37,25 +37,27 @@ function LoginPage({activeUser, onLogin}) {
     }
 
     return (
-        <Container>
-            <h3><img width="25px" src={airplane} />  Sky Flight</h3>
-            <h1>Login to continue</h1>
-            { showInvalidLogin? <Alert variant="danger">Invalid Credentails!</Alert> : null}
-            <Form>
-                <Form.Group controlId="formBasicEmail">
-                    <Form.Label>Email address</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" value={pass} onChange={(e) => setPass(e.target.value)} />
-                </Form.Group>
-                <Button variant="success" type="submit" onClick={login} block>
-                    Log In
+        <div className="p-login">
+            <Container>
+                <h3><img width="25px" src={airplane} />  Sky Flight</h3>
+                <h1>Login to continue</h1>
+                {showInvalidLogin ? <Alert variant="danger">Invalid Credentails!</Alert> : null}
+                <Form>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Email address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicPassword">
+                        <Form.Label>Password</Form.Label>
+                        <Form.Control type="password" placeholder="Password" value={pass} onChange={(e) => setPass(e.target.value)} />
+                    </Form.Group>
+                    <Button variant="success" type="submit" onClick={login} block>
+                        Log In
                 </Button>
-                <Link to="/signup" >create new account</Link>
-            </Form>
-        </Container>
+                    <Link to="/signup" >create new account</Link>
+                </Form>
+            </Container>
+        </div>
     );
 }
 
